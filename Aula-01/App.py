f8c7c63cb4c133a193ad3b8e38a4fc95623d3506
3º MTEC - Desenv. de Sistemas <ds-mtec-3@servidor.acad>
2025-02-18 11:46:31 -0300

    18/02/25

--- Aula-01/App.py
+++ Aula-01/App.py
@@ -1,8 +1,8 @@
-# Importando o Flask
-from flask import Flask
+# Importando o Flask e o render_templante (que lê o html)
+from flask import Flask, render_template
 
-# Carregando o Flask na variável app
-app = Flask(__name__)
+# Carregando o Flask na variável app. template_folder localiza a página
+app = Flask(__name__, template_folder='views')
 
 # Criando a primeira rota do site. 
     # Rota com uma '/' é a rota principal
@@ -10,7 +10,28 @@ app = Flask(__name__)
 
 # Criando função Python
 def home():
-        return '<h1> Bem-Vindo ao meu primeiro site em Flask </h1>'
+    return render_template('index.html')
+
+# Assim cria outras rotas. Deve adicionar o /nome no link da página
+@app.route('/games')
+
+
+def game():
+    titulo = 'CS:GO'
+    ano = 2012
+    categoria = 'FPS Online'
+    # Array funciona assim:
+    jogadores = ['Vinícius', 'Zezília', 'Moreira', 'Pereira']
+    jogos = ['The Sim 4', 'Lethal Comphany', 'Stardey Valley', 'FIFA 17', 'Pokemon Fire Red', 'GTA 5']
+    
+    return render_template('games.html', 
+                           titulo=titulo,
+                           ano=ano,
+                           categoria=categoria,
+                           jogadores=jogadores,
+                           jogos=jogos)
+
+
 
 if __name__ == '__main__': #Rodar caso for o principal. Quando o código está na frente do If, ele considera, caso contrario ele não considera
 
